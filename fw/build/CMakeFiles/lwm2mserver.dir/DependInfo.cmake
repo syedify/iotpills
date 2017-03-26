16f786e651142aed623409d279faf91b82d2f1e4
syedh <syedt05@gmail.com>
2017-03-26 09:50:23 -0400

    l2mem

---
+++ fw/build/CMakeFiles/lwm2mserver.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/server/lwm2mserver.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/lwm2mserver.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/block1.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/block1.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/bootstrap.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/bootstrap.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/data.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/data.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/discover.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/discover.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/er-coap-13/er-coap-13.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/er-coap-13/er-coap-13.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/json.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/json.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/liblwm2m.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/liblwm2m.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/list.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/list.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/management.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/management.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/objects.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/objects.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/observe.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/observe.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/packet.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/packet.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/registration.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/registration.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/tlv.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/tlv.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/transaction.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/transaction.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/uri.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/uri.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/utils.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/utils.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared/commandline.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared/commandline.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared/connection.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared/connection.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared/memtrace.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared/memtrace.c.o"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared/platform.c" "/mnt/f/DevLand/webportal/iotpills/fw/build/CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared/platform.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "LWM2M_LITTLE_ENDIAN"
+  "LWM2M_SERVER_MODE"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core"
+  "/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ fw/build/CMakeFiles/lwm2mserver.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/lwm2mserver.dir/lwm2mserver.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/liblwm2m.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/uri.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/utils.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/objects.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/tlv.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/data.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/list.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/packet.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/transaction.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/registration.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/bootstrap.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/management.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/observe.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/json.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/discover.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/block1.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/core/er-coap-13/er-coap-13.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared/commandline.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared/platform.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared/memtrace.c.o"
+  "CMakeFiles/lwm2mserver.dir/mnt/f/DevLand/webportal/iotpills/fw/wakaama-master/examples/shared/connection.c.o"
+  "lwm2mserver.pdb"
+  "lwm2mserver"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/lwm2mserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
